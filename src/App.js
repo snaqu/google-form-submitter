128ebc5383eba5e4c524939ee11942b5bd2129ee
pablomiklas@gmail.com <pablomiklas@gmail.com>
2019-12-30 21:37:12 +0100

    added messages and logic

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React, { useState } from "react";
 import uuidv4 from "uuid/v4";
 import produce from "immer";
 import queryString from "query-string";
+import Modal from "react-responsive-modal";
 
+import messages from "./messages/messages";
 import ConnectedInputs from "./components/molecules/connectedInputs";
 import EntryInput from "./components/atoms/entryInput";
 
 const App = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [responseMessage, setResponseMessage] = useState(messages.sending);
+  const [isResponseDone, setIsResponseDone] = useState(false);
   const [linkValue, setlLinkValue] = useState("");
-  const [submittionValue, setSubmittionValue] = useState("6");
+  const [submittionValue, setSubmittionValue] = useState("7");
   const [formValue, setFormValue] = useState([
     {
       id: uuidv4(),
@@ -50,97 +55,146 @@ const App = () => {
   ));
 
   return (
-    <div className="container mx-auto px-6 mb-10">
-      <div className="w-full my-6">
-        <h1 className="text-5xl antialiased text-center">
-          Google form submitter
-        </h1>
-        <h2 className="text-1xl antialiased my-5">
-          Works only with Google Forms which have multiple submisions allowed.
-        </h2>
-      </div>
-      <form
-        className="w-full"
-        onSubmit={e => {
-          e.preventDefault();
-          let objectWithValues = {};
-
-          formValue.forEach(({ entryValue, value }) => {
-            objectWithValues = {
-              ...objectWithValues,
-              [entryValue]: value
-            };
-          });
-
-          const query = queryString.stringify(objectWithValues);
-          const spreadsheetUrl = `https://docs.google.com/forms/${linkValue}/formResponse?${query}&submit=Submit`;
-
-          fetch(spreadsheetUrl)
-            .then(response => response.json())
-            .then(data => {
-              console.log("Success:", data);
-            })
-            .catch(error => {
-              console.error("Error:", error);
-            });
+    <>
+      <Modal
+        open={isModalOpen}
+        center
+        onClose={() => {
+          if (isResponseDone) {
+            setIsModalOpen(false);
+          }
         }}
+        closeOnEsc={isResponseDone}
+        closeOnOverlayClick={isResponseDone}
       >
-        <div className="flex flex-wrap -mx-3 mb-5">
-          <EntryInput
-            name="Link"
-            value={linkValue}
-            onChange={e => setlLinkValue(e.target.value)}
-            labelValue="Link <URL>"
-            fullWidth
-            placeholder="https://docs.google.com/forms/<URL>/viewform?usp=sf_link"
-          />
+        <h2 className="text-2xl antialiased text-center m-10">
+          {responseMessage}
+        </h2>
+      </Modal>
+      <div className="container mx-auto px-6 mb-10">
+        <div className="w-full my-6">
+          <h1 className="text-5xl antialiased text-center">
+            Google form submitter
+          </h1>
+          <h2 className="text-1xl antialiased mt-5">
+            Works only with Google Forms which have multiple submisions allowed.
+          </h2>
+          <ol className="list-decimal mx-4 my-4">
+            <li>Proceed to completing the survey</li>
+            <li>Click F12</li>
+            <li>Complete the survey and send answers</li>
+            <li>Go to the network tab in devtools</li>
+            <li>
+              Find the line with the name "form response", click on it and go to
+              the bottom
+            </li>
+            <li>In the "Form Data" tab there are answers to the form</li>
+            <li>
+              Paste them into the inputs below and select the number of
+              submittions
+            </li>
+          </ol>
         </div>
-        <div className="flex flex-wrap -mx-3 mb-5">
-          <EntryInput
-            name="Link"
-            value={submittionValue}
-            onChange={({ target }) => {
-              const pattern = /^[1-9]$/;
-              const currentValue = target.value;
+        <form
+          className="w-full"
+          onSubmit={e => {
+            e.preventDefault();
+            setIsModalOpen(true);
+            setIsResponseDone(false);
+            setResponseMessage(messages.sending);
 
-              if (currentValue === "" || pattern.test(currentValue)) {
-                setSubmittionValue(currentValue);
+            let objectWithValues = {};
+
+            formValue.forEach(({ entryValue, value }) => {
+              objectWithValues = {
+                ...objectWithValues,
+                [entryValue]: value
+              };
+            });
+
+            const query = queryString.stringify(objectWithValues);
+            const spreadsheetUrl = `https://docs.google.com/forms/${linkValue}/formResponse?${query}&submit=Submit`;
+            const corsUrl = "https://cors-anywhere.herokuapp.com/";
+
+            (async () => {
+              let promiseArray = [];
+
+              for (let i = 0; i < submittionValue; i++) {
+                const response = await fetch(`${corsUrl}${spreadsheetUrl}`);
+                if (response.ok) {
+                  promiseArray.push(response);
+                } else {
+                  setIsResponseDone(true);
+                  return setResponseMessage(messages.sendingError);
+                }
               }
-            }}
-            labelValue="Number of submittions from range (1-10)"
-            fullWidth
-            placeholder="4"
-          />
-        </div>
-        <div className="flex flex-wrap -mx-3 mb-6">{entryInputs}</div>
-        <div className="md:flex md:items-center">
-          <div className="md:w-full">
-            <button
-              className="shadow bg-purple-400 hover:bg-purple-300 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded mr-4"
-              type="button"
-              onClick={() => {
-                setFormValue(currentForm => [
-                  ...currentForm,
-                  {
-                    id: uuidv4(),
-                    entryValue: "",
-                    value: ""
-                  }
-                ]);
-              }}
-            >
-              Add input fields
-            </button>
-            <button
-              className="shadow bg-purple-800 hover:bg-purple-700 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-              type="submit"
-            >
-              Submit
-            </button>
+
+              Promise.all(promiseArray).then(() => {
+                setResponseMessage(messages.sendingCompleted);
+                setIsResponseDone(true);
+              });
+            })();
+          }}
+        >
+          <div className="flex flex-wrap -mx-3 mb-5">
+            <EntryInput
+              name="Link"
+              value={linkValue}
+              onChange={e => setlLinkValue(e.target.value)}
+              labelValue="Link <URL>"
+              fullWidth
+              placeholder="https://docs.google.com/forms/<URL>/viewform?usp=sf_link"
+            />
           </div>
-        </div>
-      </form>
-    </div>
+          <div className="flex flex-wrap -mx-3 mb-5">
+            <EntryInput
+              name="submittionValue"
+              value={submittionValue}
+              onChange={({ target }) => {
+                const pattern = /^\d*[1-9]\d*$/;
+                const currentValue = target.value;
+
+                if (currentValue === "" || pattern.test(currentValue)) {
+                  setSubmittionValue(currentValue);
+                }
+              }}
+              labelValue="Number of submittions"
+              fullWidth
+              placeholder="7"
+            />
+          </div>
+          <div className="flex flex-wrap -mx-3 mb-6">{entryInputs}</div>
+          <div className="md:flex md:items-center">
+            <div className="md:w-full">
+              <button
+                className="shadow bg-purple-400 hover:bg-purple-300 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded mr-4"
+                type="button"
+                disabled={isModalOpen}
+                onClick={() => {
+                  setFormValue(currentForm => [
+                    ...currentForm,
+                    {
+                      id: uuidv4(),
+                      entryValue: "",
+                      value: ""
+                    }
+                  ]);
+                }}
+              >
+                Add input fields
+              </button>
+              <button
+                disabled={isModalOpen}
+                className="shadow bg-purple-800 hover:bg-purple-700 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                type="submit"
+              >
+                Submit
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
   );
 };
 
---
+++ src/messages/messages.js
@@ -0,0 +1,7 @@
+const messages = {
+  sending: "Sending...",
+  sendingError: "Something went wrong! Check the link is correct.",
+  sendingCompleted: "Sending completed successfully"
+};
+
+export default messages;
